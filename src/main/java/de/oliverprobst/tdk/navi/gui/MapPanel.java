537e0ba97235bb6318f8149c81d2fb3c4f0e6f07
oprobst <OliverProbst@gmx.de>
2014-11-05 19:28:05 +0100

    Insert location only if not outside map paint area

--- src/main/java/de/oliverprobst/tdk/navi/gui/MapPanel.java
+++ src/main/java/de/oliverprobst/tdk/navi/gui/MapPanel.java
@@ -394,7 +394,9 @@ public class MapPanel extends JPanel implements PropertyChangeListener {
 			lastLongitude = newValue.getLongitude();
 			MapPoint location = hc.xyProjection(d, lastLongitude, lastLatitude);
 			location.setEstimated(true);
-			if (!locations.get(locations.size() - 1).equals(location)) {
+			if (!locations.get(locations.size() - 1).equals(location)
+					&& location.x > 0 && location.y > 0 && location.x < 480
+					&& location.y < 360) {
 				locations.add(location);
 			}
 		}
@@ -413,9 +415,12 @@ public class MapPanel extends JPanel implements PropertyChangeListener {
 			MapPoint location = hc.xyProjection(d, parser.getLongitude(),
 					parser.getLatitude());
 			if (parser.getDiluentOfPrecision() <= minDop) {
-				if (locations.isEmpty()
-						|| !locations.get(locations.size() - 1)
-								.equals(location)) {
+				if ((locations.isEmpty() || !locations
+						.get(locations.size() - 1).equals(location))
+						&& location.x > 0
+						&& location.y > 0
+						&& location.x < 480
+						&& location.y < 360) {
 					locations.add(location);
 				}
 			}
